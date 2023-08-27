632021026e8eeae0c78437f6b0b6a5279f61acc7
Gillian Reynolds <33197909+Glfrey@users.noreply.github.com>
2023-08-27 20:31:19 +0100

    Create SepUnplaced.py

---
+++ AccessoryCode/SepUnplaced.py
@@ -0,0 +1,42 @@
+#Seperates "placed" and "unplaced" sequences
+
+from Bio import SeqIO
+from Bio.SeqIO.FastaIO import SimpleFastaParser
+import argparse
+import sys
+import os
+
+
+parser = argparse.ArgumentParser(description='Extracts assembled chromosomes/contigs/scaffolds from bulk fasta file')
+parser.add_argument("-i", help='Input file',required=True)
+
+
+if len(sys.argv) < 1:
+    print("You must provide input file !", file = sys.stderr)
+    parser.print_help(sys.stderr)
+    sys.exit(1)
+
+
+args = parser.parse_args()
+i = args.i
+
+cwd = os.getcwd()
+unplaced_dirname = ("unplaced")
+placed_dirname = ("placed")
+os.mkdir(unplaced_dirname)
+os.mkdir(placed_dirname)
+
+with open(i) as input:
+    for title,seq in SimpleFastaParser(input):
+            if "unplaced" in title:
+                os.chdir(unplaced_dirname)
+                with open(title+".fa", "a+") as new:
+                    new.write(">"+title+"\n")
+                    new.write(seq)
+                    os.chdir('..')
+            else:
+                os.chdir(placed_dirname)
+                with open(title+".fa", "a+") as new:
+                    new.write(">"+title+"\n")
+                    new.write(seq)
+                    os.chdir('..')
